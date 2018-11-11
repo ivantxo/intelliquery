0c7e36f082d8337c68f6ff59bb80ac6519147bbc
ivantxo <ivan.ramirezf@gmail.com>
2018-11-11 20:12:15 +1000

    randomElement was returning an array instead of a value

--- database/seeds/MembersTableSeeder.php
+++ database/seeds/MembersTableSeeder.php
@@ -16,6 +16,7 @@ class MembersTableSeeder extends Seeder
         $faker = Factory::create();
 
         for ($i =0; $i < 1000; $i++) {
+            // randomElements returns an array wit one single element, therefore we need to add [0]
             $gender = $faker->randomElement(['male', 'female'])[0];
             Member::create([
                 'firstname' => $faker->firstName($gender),
