d92ed02ddac2210e52c407f7c24535ddbdbdd8fb
ivantxo <ivan.ramirezf@gmail.com>
2018-11-11 19:48:04 +1000

    Added basic functionality for Member model

--- app/Member.php
+++ app/Member.php
@@ -4,7 +4,21 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Member
+ * Model for the member table.
+ * @package App
+ */
 class Member extends Model
 {
-    //
+    /**
+     * Any field that is added to this array is whitelisted for mass assignment
+     * @var array
+     */
+    protected $fillable = [
+        'firstname',
+        'surname',
+        'email',
+        'gender',
+    ];
 }
