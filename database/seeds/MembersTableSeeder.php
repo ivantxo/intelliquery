90744173f180fb03fcf7a0b2504c75a844025131
ivantxo <ivan.ramirezf@gmail.com>
2018-11-11 19:58:36 +1000

    Acconditionate seeder to seed members table

--- database/seeds/MembersTableSeeder.php
+++ database/seeds/MembersTableSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Member;
+use Faker\Factory;
 use Illuminate\Database\Seeder;
 
 class MembersTableSeeder extends Seeder
@@ -11,6 +13,17 @@ class MembersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $faker = Factory::create();
+
+        for ($i =0; $i < 1000; $i++) {
+            $gender = $faker->randomElement(['male', 'female'])[0];
+            Member::create([
+                'firstname' => $faker->firstName($gender),
+                'surname' => $faker->lastName,
+                'email' => $faker->email,
+                'gender' => $gender === 'm' ? 'Male' : 'Female',
+                'created_at' => $faker->dateTime(),
+            ]);
+        }
     }
 }
